b9fd18e52353b69b92bac7d7addbb0b2544cf994
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-11-24 22:33:37 +0200

    test downlaod file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-// dynamic(() => import("./_components/firstDashboard"), { ssr: false });
+dynamic(() => import("./_components/firstDashboard"), { ssr: false });
 const FirstDashboard = dynamic(
   () => import("./_components/firstDashboard"),
   { ssr: false }
@@ -10,6 +10,9 @@ export default function Home() {
   return (
     <>
       <FirstDashboard />
+      <a href="/FIISaudi.xlsx" download="FIISaudi.xlsx">
+        <button className="p-5 bg-amber-900 rounded-2xl text-2xl text-center cursor-pointer">Download File</button>
+      </a>
     </>
   );
 }
