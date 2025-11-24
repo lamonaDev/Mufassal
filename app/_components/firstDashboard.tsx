463691e9b947eafe7cc87c12c43f1c7ba8af73bc
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-11-25 00:31:11 +0200

    testing dashboard

--- app/_components/firstDashboard.tsx
+++ app/_components/firstDashboard.tsx
@@ -1,11 +1,18 @@
-
+import dynamic from "next/dynamic";
+import Image from "next/image";
 function FirstDashboard() {
     return (
         <>
-            <iframe title="TEST" width="600" height="373.5"
-                src="https://app.powerbi.com/view?r=eyJrIjoiOWE0ZDVjMmEtZmQzYy00NmYyLWIxZjgtOGQyMjM1M2EyYmE5IiwidCI6ImVhZjYyNGM4LWEwYzQtNDE5NS04N2QyLTQ0M2U1ZDc1MTZjZCIsImMiOjh9"
-                frameBorder="0" allowFullScreen={true}></iframe>
+            <iframe
+            title="TEST"
+            width="600"
+            height="373.5"
+            src="https://app.powerbi.com/view?r=eyJrIjoiOWE0ZDVjMmEtZmQzYy00NmYyLWIxZjgtOGQyMjM1M2EyYmE5IiwidCI6ImVhZjYyNGM4LWEwYzQtNDE5NS04N2QyLTQ0M2U1ZDc1MTZjZCIsImMiOjh9"
+            frameBorder="0"
+            allowFullScreen={true}
+            ></iframe>
         </>
     );
 }
+
 export default FirstDashboard;
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const FirstDashboard = dynamic(
 export default function Home() {
   return (
     <>
-      <FirstDashboard />
+    <FirstDashboard />
       <a href="/FIISaudi.xlsx" download="FIISaudi.xlsx">
         <button className="p-5 bg-amber-900 rounded-2xl text-2xl text-center cursor-pointer">Download File</button>
       </a>
