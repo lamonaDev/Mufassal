bb1bc26f1d7d8970cd90b644b4ed42577731104f
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-11-24 22:08:36 +0200

    test add dashboard

---
+++ app/_components/firstDashboard.tsx
@@ -0,0 +1,11 @@
+
+function FirstDashboard() {
+    return (
+        <>
+            <iframe title="TEST" width="600" height="373.5"
+                src="https://app.powerbi.com/view?r=eyJrIjoiOWE0ZDVjMmEtZmQzYy00NmYyLWIxZjgtOGQyMjM1M2EyYmE5IiwidCI6ImVhZjYyNGM4LWEwYzQtNDE5NS04N2QyLTQ0M2U1ZDc1MTZjZCIsImMiOjh9"
+                frameBorder="0" allowFullScreen={true}></iframe>
+        </>
+    );
+}
+export default FirstDashboard;
