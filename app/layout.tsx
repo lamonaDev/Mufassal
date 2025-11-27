cc9eee17a07305f585cccb63f492b44cf0bef7ea
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-11-27 11:42:12 +0200

    add verfication to track website data

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="DTjnT0AFssM6Ek-3HwXDdJIL_zrpni03KOi0gi7cOOg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
