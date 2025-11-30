9f4391b662d8b810aa025befa09b0a113acde094
Lamona <Lamona@gmail.com>
2025-11-30 14:57:30 +0200

    see the documentation sidebar

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,141 +1,227 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-import { Ellipsis } from 'lucide-react';
+import * as React from "react"
+import { GalleryVerticalEnd, Minus, Plus } from "lucide-react"
+
+import { SearchForm } from "@/components/search-form"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
 import {
     Sidebar,
     SidebarContent,
     SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
+    SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
     SidebarMenuSub,
     SidebarMenuSubButton,
     SidebarMenuSubItem,
+    SidebarRail,
 } from "@/components/ui/sidebar"
-import Link from "next/link"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
-const items = [
-    {
-        title: "Home",
-        url: "#",
-        icon: Home,
-    },
-    {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
-    },
-    {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
-    },
-]
 
-export default function AppSidebar() {
+// This is sample data.
+const data = {
+    navMain: [
+        {
+            title: "Getting Started",
+            url: "#",
+            items: [
+                {
+                    title: "Installation",
+                    url: "#",
+                },
+                {
+                    title: "Project Structure",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Building Your Application",
+            url: "#",
+            items: [
+                {
+                    title: "Routing",
+                    url: "#",
+                },
+                {
+                    title: "Data Fetching",
+                    url: "#",
+                    isActive: true,
+                },
+                {
+                    title: "Rendering",
+                    url: "#",
+                },
+                {
+                    title: "Caching",
+                    url: "#",
+                },
+                {
+                    title: "Styling",
+                    url: "#",
+                },
+                {
+                    title: "Optimizing",
+                    url: "#",
+                },
+                {
+                    title: "Configuring",
+                    url: "#",
+                },
+                {
+                    title: "Testing",
+                    url: "#",
+                },
+                {
+                    title: "Authentication",
+                    url: "#",
+                },
+                {
+                    title: "Deploying",
+                    url: "#",
+                },
+                {
+                    title: "Upgrading",
+                    url: "#",
+                },
+                {
+                    title: "Examples",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "API Reference",
+            url: "#",
+            items: [
+                {
+                    title: "Components",
+                    url: "#",
+                },
+                {
+                    title: "File Conventions",
+                    url: "#",
+                },
+                {
+                    title: "Functions",
+                    url: "#",
+                },
+                {
+                    title: "next.config.js Options",
+                    url: "#",
+                },
+                {
+                    title: "CLI",
+                    url: "#",
+                },
+                {
+                    title: "Edge Runtime",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Architecture",
+            url: "#",
+            items: [
+                {
+                    title: "Accessibility",
+                    url: "#",
+                },
+                {
+                    title: "Fast Refresh",
+                    url: "#",
+                },
+                {
+                    title: "Next.js Compiler",
+                    url: "#",
+                },
+                {
+                    title: "Supported Browsers",
+                    url: "#",
+                },
+                {
+                    title: "Turbopack",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Community",
+            url: "#",
+            items: [
+                {
+                    title: "Contribution Guide",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+}
+export default function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
-        <Sidebar side="left">
+        <Sidebar {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <a href="#">
+                                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                                    <GalleryVerticalEnd className="size-4" />
+                                </div>
+                                <div className="flex flex-col gap-0.5 leading-none">
+                                    <span className="font-medium">Documentation</span>
+                                    <span className="">v1.0.0</span>
+                                </div>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+                <SearchForm />
+            </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <Link href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </Link>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <Link href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </Link>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                    <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            <Collapsible className="group/collapsible">
+                    <SidebarMenu>
+                        {data.navMain.map((item, index) => (
+                            <Collapsible
+                                key={item.title}
+                                defaultOpen={index === 1}
+                                className="group/collapsible"
+                            >
                                 <SidebarMenuItem>
                                     <CollapsibleTrigger asChild>
-                                            <SidebarMenuButton tooltip="Expand Settings" className="font-black">Settings <Ellipsis /></SidebarMenuButton>
+                                        <SidebarMenuButton>
+                                            {item.title}{" "}
+                                            <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                                            <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                                        </SidebarMenuButton>
                                     </CollapsibleTrigger>
-                                    <CollapsibleContent>
-                                        <SidebarMenuSub>
-                                            <SidebarMenuSubItem>
-                                                <SidebarMenuSubButton asChild>
-                                                    <a href="/profile">Profile</a>
-                                                </SidebarMenuSubButton>
-                                            </SidebarMenuSubItem>
-                                            <SidebarMenuSubItem>
-                                                <SidebarMenuSubButton asChild>
-                                                    <a href="/notifications">Notifications</a>
-                                                </SidebarMenuSubButton>
-                                            </SidebarMenuSubItem>
-                                            <SidebarMenuSubItem>
-                                                    <Collapsible  className="group/collabsible">
-                                                        <SidebarMenuItem>
-                                                            <CollapsibleTrigger asChild>
-                                                                <SidebarMenuButton tooltip="Expand Settings">Another Section <Ellipsis /></SidebarMenuButton>
-                                                            </CollapsibleTrigger>
-                                                            <CollapsibleContent>
-                                                                <SidebarMenuSub>
-                                                                    <SidebarMenuSubItem>
-                                                                        <SidebarMenuButton asChild>
-                                                                            <a href="#">one</a>
-                                                                        </SidebarMenuButton>
-                                                                    </SidebarMenuSubItem>
-                                                                    <SidebarMenuSubItem>
-                                                                        <SidebarMenuButton asChild>
-                                                                            <a href="#">two</a>
-                                                                        </SidebarMenuButton>
-                                                                    </SidebarMenuSubItem>
-                                                                    <SidebarMenuSubItem>
-                                                                        <SidebarMenuButton asChild>
-                                                                            <a href="#">three</a>
-                                                                        </SidebarMenuButton>
-                                                                    </SidebarMenuSubItem>
-                                                                </SidebarMenuSub>
-                                                            </CollapsibleContent>
-                                                        </SidebarMenuItem>
-                                                    </Collapsible>
-                                            </SidebarMenuSubItem>
-                                        </SidebarMenuSub>
-                                    </CollapsibleContent>
+                                    {item.items?.length ? (
+                                        <CollapsibleContent>
+                                            <SidebarMenuSub>
+                                                {item.items.map((item) => (
+                                                    <SidebarMenuSubItem key={item.title}>
+                                                        <SidebarMenuSubButton
+                                                            asChild
+                                                            isActive={item.isActive}
+                                                        >
+                                                            <a href={item.url}>{item.title}</a>
+                                                        </SidebarMenuSubButton>
+                                                    </SidebarMenuSubItem>
+                                                ))}
+                                            </SidebarMenuSub>
+                                        </CollapsibleContent>
+                                    ) : null}
                                 </SidebarMenuItem>
                             </Collapsible>
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
+                        ))}
+                    </SidebarMenu>
                 </SidebarGroup>
             </SidebarContent>
+            <SidebarRail />
         </Sidebar>
     )
 }
---
+++ notes.md
@@ -0,0 +1,7 @@
+## see the base structure
+## shadcn components
+## nesting the sidebar documentation
+## see apex charts and shadcn dashboard implemetation
+## talk about the responsiveines of the main webiste
+## talk about the breadcrumbs to the navigation inside the main webiste
+## react hot toast for user interactions
